487611190af246dd8e1690f214157ab004a94c68
Kim <cuddlelyholly@naver.com>
2024-06-16 22:47:05 +0900

    정올 반복제어문 3

--- 반복제어문1/FE4.java
+++ 반복제어문1/FE4.java
@@ -15,3 +15,18 @@ public class FE4 {
         }
 }
 
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 1; j < col + 1; j++) {
+                System.out.printf("%d ",i+j);
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ 반복제어문3/P2.java
@@ -0,0 +1,11 @@
+package 반복제어문3;
+
+public class P2 {
+    public static void main(String[] args) {
+        String a = "*";
+        for(int i = 1; i <4; i++) {
+            System.out.println(a.repeat(i));
+        }
+    }
+}
+
