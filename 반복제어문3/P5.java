8c518ef516b1358ad237ea108d656e17dbd5ee38
Kim <cuddlelyholly@naver.com>
2024-06-16 23:27:46 +0900

    반복제어문5-2

--- 반복제어문3/P5.java
+++ 반복제어문3/P5.java
@@ -1,5 +1,7 @@
 package 반복제어문3;
 
+import java.util.Scanner;
+
 public class P5 {
     public static void main(String[] args) {
         String s = "*";
@@ -9,3 +11,23 @@ public class P5 {
         }
     }
 }
+
+class Main {
+    public static void main(String[] args) {
+        String s = "*";
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            System.out.print(" ".repeat(n-i));
+            System.out.print(s.repeat((i*2)-1));
+            System.out.println(" ".repeat(n-i));
+        }
+    }
+}
+//공백은
+//0
+//1
+//2
+// -3 -i
+//
