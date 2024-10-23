44fb3419302986b6bfcd1b5b4551c23639bd100d
KeK <127266806+disneydreamworker@users.noreply.github.com>
2024-10-23 18:27:38 +0900

    :sparkles: 완전탐색 문제 10

---
+++ LC_Java/완전탐색/완전탐색_241022_15593_Lifeguards.java
@@ -0,0 +1,26 @@
+package LC_Java.완전탐색;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 완전탐색_241022_15593_Lifeguards {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt() - 1;
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int index = 0;
+        int[] start = new int[0];
+        int[] end;
+        
+        while(br.readLine() != null) {
+            start[index] = 0;
+        }
+
+    }
+}
